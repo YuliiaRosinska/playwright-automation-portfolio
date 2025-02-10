64508b2e489ff0ab8bc676bd6cd673901997ace2
Yuliia <80529514+JuliaRosinska@users.noreply.github.com>
2025-02-10 12:59:25 +0000

    fix playwright configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : 1,
-  reporter: 'list',
+  workers: 1,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     baseURL: 'https://www.saucedemo.com/',
     trace: 'on-first-retry',
     testIdAttribute: 'data-test',
-    headless: false,
+    headless: true,
   },
 })
